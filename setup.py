3524a220587b793546e2a431583bcf804aab071f
masatoEmata <jng.bosn@gmail.com>
2023-06-26 03:05:19 +0900

    add README

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="gcloud-secret-configure",
-    version="0.0.2",
+    version="1.0.0",
     description="A package to configure environment variables from Google Cloud Secret Manager",
     packages=find_packages(),
     install_requires=[],
     classifiers=[  # https://pypi.org/classifiers/
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
